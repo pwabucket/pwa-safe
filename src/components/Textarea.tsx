ec53a823d7f24f5c9cd29775649840736e3df895
Sadiq Salau <sadiqsalau888@gmail.com>
2025-08-12 08:51:01 +0100

    feat: Replaced Input component with auto resized textarea

--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -1,7 +1,20 @@
-import Input from "./Input";
+import TextareaAutosize from "react-textarea-autosize";
 
-const Textarea = (props: React.ComponentPropsWithoutRef<"textarea">) => {
-  return <Input as="textarea" {...props} />;
+import { cn } from "../lib/utils";
+
+const Textarea = (
+  props: React.ComponentPropsWithoutRef<typeof TextareaAutosize>
+) => {
+  return (
+    <TextareaAutosize
+      {...props}
+      className={cn(
+        "p-2 text-green-100",
+        "border border-green-500 focus:outline-0 focus:bg-neutral-700",
+        props.className
+      )}
+    />
+  );
 };
 
 export default Textarea;
