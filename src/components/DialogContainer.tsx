c5c995a4a7cc5f12196cdb96cb3a50e708b5e759
Sadiq Salau <sadiqsalau888@gmail.com>
2025-08-12 17:41:32 +0100

    fix: Set minimum width and height to 0

--- src/components/DialogContainer.tsx
+++ src/components/DialogContainer.tsx
@@ -14,7 +14,7 @@ export default function DialogContainer(
             "relative",
             "flex flex-col gap-2 p-4",
             "border border-green-600 bg-green-900 text-green-200",
-            "w-full max-w-sm",
+            "w-full max-w-sm min-w-0 min-h-0",
             props.className
           )}
         />
