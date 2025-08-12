f3af68024bc649ae50bd6b823d53e4599b73b5c5
Sadiq Salau <sadiqsalau888@gmail.com>
2025-08-12 17:38:54 +0100

    feat: Used Paragraph Tag

--- src/components/EntryFormFileContent.tsx
+++ src/components/EntryFormFileContent.tsx
@@ -5,12 +5,12 @@ import Card from "./Card";
 export const EntryFormFileContent = memo(({ content }: { content: File }) => {
   return (
     <Card className="flex flex-col gap-2">
-      <span className="text-sm text-center text-green-100 truncate">
+      <p className="text-sm text-center text-green-100 truncate">
         {content.name}
-      </span>
-      <span className="text-xs text-center text-green-500 truncate">
+      </p>
+      <p className="text-xs text-center text-green-500 truncate">
         {content.type}
-      </span>
+      </p>
     </Card>
   );
 });
