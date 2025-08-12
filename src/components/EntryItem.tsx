8a8d56dbd19cdf9a3ca743170c14ae72203afe56
Sadiq Salau <sadiqsalau888@gmail.com>
2025-08-12 17:24:07 +0100

    feat: Align items to center and truncate title

--- src/components/EntryItem.tsx
+++ src/components/EntryItem.tsx
@@ -5,8 +5,12 @@ import type { Entry } from "../types/entry";
 
 export default function EntryItem({ entry }: { entry: Entry }) {
   return (
-    <Button as={Link} to={`/entries/${entry.id}`} className="flex gap-2">
-      <h2 className="text-green-300 grow min-w-0">{entry.title}</h2>
+    <Button
+      as={Link}
+      to={`/entries/${entry.id}`}
+      className="flex gap-2 items-center"
+    >
+      <h2 className="text-green-300 grow min-w-0 truncate">{entry.title}</h2>
       <p className="text-xs text-green-100 uppercase">{entry.type}</p>
     </Button>
   );
