eb5e8aa7213781ba7d68907e280821151af8ed66
Sadiq Salau <sadiqsalau888@gmail.com>
2025-08-12 16:25:31 +0100

    feat: Encrypt content passed from Window Opener

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -7,6 +7,7 @@ import AppLayout from "../layouts/AppLayout";
 import Card from "../components/Card";
 import EntryItem from "../components/EntryItem";
 import Input from "../components/Input";
+import WindowOpenerHandler from "../components/WindowOpenerHandler";
 import useAppStore from "../store/useAppStore";
 import { HeaderButton } from "../layouts/HeaderButton";
 import { searchProperties } from "../lib/utils";
@@ -19,47 +20,50 @@ export default function Dashboard() {
     [entries, search]
   );
   return (
-    <AppLayout
-      headerLeftContent={
-        <HeaderButton icon={HiOutlineBars3} as={Link} to="/menu" />
-      }
-      headerRightContent={
-        <HeaderButton icon={HiOutlinePlus} as={Link} to="/entries/create" />
-      }
-      className="gap-4"
-    >
-      <div className="grid grid-cols-3">
-        <Card className="flex flex-col gap-2">
-          <span className="text-green-300 text-2xl">{entries.length}</span>
-          <p className="text-green-100 text-xs">
-            <ReactTyped
-              strings={["Entries"]}
-              typeSpeed={20}
-              showCursor={false}
-            />
+    <>
+      <WindowOpenerHandler />
+      <AppLayout
+        headerLeftContent={
+          <HeaderButton icon={HiOutlineBars3} as={Link} to="/menu" />
+        }
+        headerRightContent={
+          <HeaderButton icon={HiOutlinePlus} as={Link} to="/entries/create" />
+        }
+        className="gap-4"
+      >
+        <div className="grid grid-cols-3">
+          <Card className="flex flex-col gap-2">
+            <span className="text-green-300 text-2xl">{entries.length}</span>
+            <p className="text-green-100 text-xs">
+              <ReactTyped
+                strings={["Entries"]}
+                typeSpeed={20}
+                showCursor={false}
+              />
+            </p>
+          </Card>
+        </div>
+
+        {entries.length === 0 ? (
+          <p className="bg-green-500/5 text-green-500 p-4 text-center">
+            No entries found. Click the button above to create your first entry.
           </p>
-        </Card>
-      </div>
+        ) : (
+          <>
+            <Input
+              placeholder="Search entries..."
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
 
-      {entries.length === 0 ? (
-        <p className="bg-green-500/5 text-green-500 p-4 text-center">
-          No entries found. Click the button above to create your first entry.
-        </p>
-      ) : (
-        <>
-          <Input
-            placeholder="Search entries..."
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
-
-          <div className="flex flex-col gap-4">
-            {filteredEntries.map((entry) => (
-              <EntryItem key={entry.id} entry={entry} />
-            ))}
-          </div>
-        </>
-      )}
-    </AppLayout>
+            <div className="flex flex-col gap-4">
+              {filteredEntries.map((entry) => (
+                <EntryItem key={entry.id} entry={entry} />
+              ))}
+            </div>
+          </>
+        )}
+      </AppLayout>
+    </>
   );
 }
