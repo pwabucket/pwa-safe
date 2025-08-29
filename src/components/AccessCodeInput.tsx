2d362984e6c71dd1ba0926c4eb6eb8accc7841c8
Sadiq Salau <sadiqsalau888@gmail.com>
2025-08-29 17:46:56 +0100

    feat: Toggle between PIN or password

--- src/components/AccessCodeInput.tsx
+++ src/components/AccessCodeInput.tsx
@@ -1,12 +1,37 @@
 import { useCallback, useState } from "react";
-import Keypad from "./Keypad";
+
 import AccessCodeOTPInput from "./AccessCodeOTPInput";
+import Button from "./Button";
+import Keypad from "./Keypad";
+import PasswordInput from "./PasswordInput";
+import useAppStore from "../store/useAppStore";
+
+const AccessCodeInputTypeToggle = () => {
+  const accessCodeInputType = useAppStore((state) => state.accessCodeInputType);
+  const toggleAccessCodeInputType = useAppStore(
+    (state) => state.toggleAccessCodeInputType
+  );
+
+  return (
+    <div className="flex justify-end">
+      <button
+        type="button"
+        onClick={toggleAccessCodeInputType}
+        className="border border-green-500 cursor-pointer text-xs p-1 text-green-500"
+      >
+        Use {accessCodeInputType === "pin" ? "Password" : "PIN"}
+      </button>
+    </div>
+  );
+};
 
 export default function AccessCodeInput({
   onFilled,
 }: {
   onFilled?: (value: string) => void;
 }) {
+  const accessCodeInputType = useAppStore((state) => state.accessCodeInputType);
+
   const [input, setInput] = useState("");
 
   const updateInput = useCallback(
@@ -34,15 +59,37 @@ export default function AccessCodeInput({
     []
   );
 
-  return (
-    <div className="flex flex-col gap-4">
-      <AccessCodeOTPInput input={input} updateInput={updateInput} />
+  const submitAccessCode = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (input) {
+      onFilled?.(input);
+    }
+  };
 
-      <Keypad
-        onInput={handleKeypadInput}
-        onClear={clearInput}
-        onDelete={deleteInput}
-      />
-    </div>
+  return (
+    <form onSubmit={submitAccessCode} className="flex flex-col gap-4">
+      {accessCodeInputType === "pin" ? (
+        <>
+          <AccessCodeInputTypeToggle />
+          <AccessCodeOTPInput input={input} updateInput={updateInput} />
+          <Keypad
+            onInput={handleKeypadInput}
+            onClear={clearInput}
+            onDelete={deleteInput}
+          />
+        </>
+      ) : (
+        <>
+          <div className="flex flex-col gap-1">
+            <AccessCodeInputTypeToggle />
+            <PasswordInput
+              onChange={(e) => setInput(e.target.value)}
+              placeholder="Access Code"
+            />
+          </div>
+          <Button type="submit">Submit</Button>
+        </>
+      )}
+    </form>
   );
 }
