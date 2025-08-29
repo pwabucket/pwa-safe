ceeab5746786c4140c6c68ee0be165686b73d8d4
Sadiq Salau <sadiqsalau888@gmail.com>
2025-08-29 18:02:54 +0100

    feat: Imported access code input toggle component

--- src/components/AccessCodeInput.tsx
+++ src/components/AccessCodeInput.tsx
@@ -1,29 +1,11 @@
 import { useCallback, useState } from "react";
 
+import AccessCodeInputTypeToggle from "./AccessCodeInputTypeToggle";
 import AccessCodeOTPInput from "./AccessCodeOTPInput";
 import Button from "./Button";
 import Keypad from "./Keypad";
 import PasswordInput from "./PasswordInput";
 import useAppStore from "../store/useAppStore";
-
-const AccessCodeInputTypeToggle = () => {
-  const accessCodeInputType = useAppStore((state) => state.accessCodeInputType);
-  const toggleAccessCodeInputType = useAppStore(
-    (state) => state.toggleAccessCodeInputType
-  );
-
-  return (
-    <div className="flex justify-end">
-      <button
-        type="button"
-        onClick={toggleAccessCodeInputType}
-        className="border border-green-500 cursor-pointer text-xs p-1 text-green-500"
-      >
-        Use {accessCodeInputType === "pin" ? "Password" : "PIN"}
-      </button>
-    </div>
-  );
-};
 
 export default function AccessCodeInput({
   onFilled,
