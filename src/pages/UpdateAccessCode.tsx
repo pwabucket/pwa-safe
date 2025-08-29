eefecdb757437364898e7e97c95d26f83f3d2bfb
Sadiq Salau <sadiqsalau888@gmail.com>
2025-08-29 18:04:03 +0100

    feat: Allow toggling between PIN or password

--- src/pages/UpdateAccessCode.tsx
+++ src/pages/UpdateAccessCode.tsx
@@ -1,12 +1,31 @@
 import { useNavigate } from "react-router";
 import { useState } from "react";
+
+import AccessCodeInputTypeToggle from "../components/AccessCodeInputTypeToggle";
 import AccessCodeOTPInput from "../components/AccessCodeOTPInput";
 import Button from "../components/Button";
 import InnerAppLayout from "../layouts/InnerAppLayout";
+import PasswordInput from "../components/PasswordInput";
 import ProcessDialog from "../components/ProcessDialog";
 import safe from "../services/safe";
 import useAppStore from "../store/useAppStore";
 import useDialogManager from "../hooks/useDialogManager";
+
+const AccessCodeUpdateInput = ({
+  value,
+  setValue,
+}: {
+  value: string;
+  setValue: (newValue: string) => void;
+}) => {
+  const accessCodeInputType = useAppStore((state) => state.accessCodeInputType);
+
+  return accessCodeInputType === "pin" ? (
+    <AccessCodeOTPInput input={value} updateInput={setValue} />
+  ) : (
+    <PasswordInput value={value} onChange={(e) => setValue(e.target.value)} />
+  );
+};
 
 export default function UpdateAccessCode() {
   const entries = useAppStore((state) => state.entries);
@@ -42,16 +61,21 @@ export default function UpdateAccessCode() {
 
   return (
     <InnerAppLayout headerTitle="Update Access Code" className="gap-4">
-      <h3 className="text-xs text-green-300">Current Access Code</h3>
-      <AccessCodeOTPInput
-        input={currentAccessCode}
-        updateInput={setCurrentAccessCode}
+      {/* Current Access Code */}
+      <div className="flex items-center justify-between gap-4">
+        <h3 className="text-xs text-green-300">Current Access Code</h3>
+        <AccessCodeInputTypeToggle />
+      </div>
+      <AccessCodeUpdateInput
+        value={currentAccessCode}
+        setValue={setCurrentAccessCode}
       />
 
+      {/* New Access Code */}
       <h3 className="text-xs text-green-300">New Access Code</h3>
-      <AccessCodeOTPInput
-        input={newAccessCode}
-        updateInput={setNewAccessCode}
+      <AccessCodeUpdateInput
+        value={newAccessCode}
+        setValue={setNewAccessCode}
       />
 
       <Button onClick={updateAccessCode}>Update Access Code</Button>
