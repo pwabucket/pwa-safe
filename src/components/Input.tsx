3e2bbb0bcd6185405b945184b3c423a8ddac73ca
Sadiq Salau <sadiqsalau888@gmail.com>
2026-03-08 04:17:13 +0100

    fix: Update autofill caret and text color for improved visibility

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -11,9 +11,9 @@ const Input: DynamicComponent<"input"> = ({ as, ...props }) => {
         "bg-neutral-700 focus:outline-none",
         "disabled:opacity-50",
 
-        "autofill:bg-clip-text autofill:caret-white",
+        "autofill:bg-clip-text autofill:caret-green-500",
         "autofill:inset-ring-30 autofill:inset-ring-neutral-700",
-        "autofill:[-webkit-text-fill-color:var(--color-white)]",
+        "autofill:[-webkit-text-fill-color:var(--color-green-500)]",
         props.className,
       )}
     />
